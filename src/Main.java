aa4481670435a41e586259634a9477d8a344cda6
sapthaa <andre.saptha@hotmail.com>
2023-10-04 17:51:45 +0200

    Lektion 6 Uppgift

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] premierLeague = {"Juventus", "Barcelona", "Cristiano Ronaldo", "Lionel Messi"};
+
+        for (int i = 0; i <premierLeague.length; i++){
+            System.out.println(premierLeague[i]);
+
+
+
+        }
+
+
     }
+
+
+
 }
+
+
+
+
